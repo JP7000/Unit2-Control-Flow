a08d52cd764f8a34aca3c99c8ae9a8b70d1c6d13
Justin <juspua3@gmail.com>
2025-10-09 11:57:52 -0400

    Done

---
+++ Lesson6-For Loops/practice.py
@@ -0,0 +1,9 @@
+# number = 5
+# for i in range(1,11):
+#     result = number * i
+#     print(f"{number} * {i} = {result}")
+word = "Python"
+reverse = ''
+for letter in word:
+    reverse = letter + reverse
+print(reverse)
