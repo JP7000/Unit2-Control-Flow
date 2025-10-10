96cdb5990fbff2e6ad4dd38545a1a6088ab7821d
JP7000 <juspua3@gmail.com>
2025-10-10 14:35:05 +0000

    almost done

--- Lesson6-For Loops/practice.py
+++ Lesson6-For Loops/practice.py
@@ -7,3 +7,29 @@ reverse = ''
 for letter in word:
     reverse = letter + reverse
 print(reverse)
+#number 11
+for i in range(10,0,-1):
+    print(i)
+print("Blast off!")
+
+#number 12
+text = "hello world"
+count = 0
+for char in text:
+    if char.lower() == "o"
+    count += 1
+print(count)
+
+#number 13
+pass = "Pass123"
+is_upper = 0
+is_lower = 0
+is_digit = 0
+for char in pass:
+    if char.isupper():
+        is_upper = 1
+    elif char.islower():
+        is_lower = 1
+    elif char.isdigit():
+        is_digit = 1
+is_va
